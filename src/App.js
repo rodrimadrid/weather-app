01bd6f1473b0fbd323e51ab273ca01293362b6dd
rodrimadrid <77419107+rodrimadrid@users.noreply.github.com>
2021-12-17 17:04:35 -0300

    final changes

--- src/App.js
+++ src/App.js
@@ -1,76 +1,78 @@
-import React, {useState, useEffect} from "react";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Searchbar from './components/Searchbar';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Searchbar from "./components/Searchbar";
 import WeatherView from "./components/WeatherView";
 
 function App() {
-const [city, setCity] = useState()
-const [currentWeather, setCurrentWeather] = useState()
-const [forecast, setForecast] = useState()
-const [coord, setCoord] = useState()
- const apiKey = 'cfc0bb2a30e6c158d818575f9fd77655'
- 
+  const [city, setCity] = useState();
+  const [currentWeather, setCurrentWeather] = useState();
+  const [forecast, setForecast] = useState();
+  const [coord, setCoord] = useState();
 
- async function getWeather(URL){
-  if (URL === 1) {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=sp&units=metric&appid=${apiKey}`)
-    let datoscurrentWeather = await response.json()
-    setCurrentWeather(datoscurrentWeather)
-    setCoord(datoscurrentWeather.coord)
-  } else if (URL === 2) {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&units=metric&exclude=hourly,minutely&appid=${apiKey}`)
-    let datoscurrentWeather = await response.json()
-    setForecast(datoscurrentWeather)
+  const apiKey = "cfc0bb2a30e6c158d818575f9fd77655";
+
+  async function getWeather(URL) {
+    if (URL === 1) {
+      let response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=sp&units=metric&appid=${apiKey}`
+      );
+      let datoscurrentWeather = await response.json();
+      setCurrentWeather(datoscurrentWeather);
+      setCoord(datoscurrentWeather.coord);
+    } else if (URL === 2) {
+      let response = await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&units=metric&exclude=hourly,minutely&appid=${apiKey}`
+      );
+      let datoscurrentWeather = await response.json();
+      setForecast(datoscurrentWeather);
+    } else if (URL === 3) {
+      let response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lon}&lang=sp&units=metric&appid=${apiKey}`
+      );
+      let datoscurrentWeather = await response.json();
+      setCurrentWeather(datoscurrentWeather);
+    }
   }
-  else if (URL === 3) {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lon}&lang=sp&units=metric&appid=${apiKey}`)
-    let datoscurrentWeather = await response.json()
-    setCurrentWeather(datoscurrentWeather)
-  }
-}
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    let input = e.target.firstChild;
+    setCity(input.value);
   
- const handleSearch=(e)=>{
-    e.preventDefault()
-    let input = e.target.firstChild
-    setCity(input.value)
-}
-const getLocation = () => {
-  navigator.geolocation
-    ? navigator.geolocation.getCurrentPosition(position)
-    : console.log("ubicacion no disponible");
-};
-const position = (pos)=>{
-  setCoord({
-    lon: pos.coords.longitude,
-    lat: pos.coords.latitude
-  })
-}
-useEffect(() => {
-  getLocation()
-  
-}, [])
-useEffect(() => {
- if (coord) {
-   getWeather(2)
- }
-}, [coord])
-useEffect(() => {
-if (city) {
-  getWeather(1)
-}else if (!city && coord) {
-  getWeather(3)
-}
-}, [city, coord])
-/* useEffect(() => {
-  getWeather(1)
-}, [city]) */
-console.log(forecast);
-console.log(currentWeather.name);
+  };
+  const getLocation = () => {
+    navigator.geolocation
+      ? navigator.geolocation.getCurrentPosition(position)
+      : console.log("ubicacion no disponible");
+  };
+  const position = (pos) => {
+    setCoord({
+      lon: pos.coords.longitude,
+      lat: pos.coords.latitude,
+    });
+  };
+  useEffect(() => {
+    getLocation();
+  }, []);
+  useEffect(() => {
+   coord ?  getWeather(2) : console.log('coords not found')
+  }, [coord]);
+  useEffect(() => {
+    if (!city && coord) {
+      getWeather(3);
+    }
+  }, [city, coord]);
+  useEffect(() => {
+    city? getWeather(1) : console.log('city not found');
+  }, [city])
+
   return (
     <div className="App">
       <Searchbar handleSearch={handleSearch} />
-      {(forecast && currentWeather) &&  <WeatherView currentWeather={currentWeather} forecast={forecast} />}
+      {(forecast && currentWeather) && (
+        <WeatherView currentWeather={currentWeather} forecast={forecast} />
+      )}
       {!currentWeather && <h1>Cargando...</h1>}
     </div>
   );
